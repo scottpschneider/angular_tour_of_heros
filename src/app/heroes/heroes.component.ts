86c253dd31fd70eaf663c89e129ce293ed1faac8
Scott <schneider_scott@live.com>
2018-07-30 20:54:53 -0600

    selectedHeroes

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -13,6 +13,11 @@ export class HeroesComponent implements OnInit {
     id: 1, 
     name: 'Windstorm'
   };
+  selectedHero: Hero;
+
+  onSelect(hero:Hero):void{
+    this.selectedHero=hero;
+  }
 
   constructor() { }
 
