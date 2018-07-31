3738dfb34bf00670f77b1bea7807eb05441c8539
Scott <schneider_scott@live.com>
2018-07-30 21:06:59 -0600

    fixed styles and capital lowercase error

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -9,18 +9,14 @@ import { HEROES } from '../mock-heroes';
 })
 export class HeroesComponent implements OnInit {
   heroes = HEROES;
-  hero: Hero = {
-    id: 1, 
-    name: 'Windstorm'
-  };
   selectedHero: Hero;
-
-  onSelect(hero:Hero):void{
-    this.selectedHero=hero;
-  }
-
+ 
   constructor() { }
 
   ngOnInit() {
+  }
+  
+  onSelect(hero:Hero):void{
+    this.selectedHero=hero;
   }
 }
